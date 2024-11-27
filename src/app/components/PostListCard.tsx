3d48b9f7a08710699a77605e7419ca3a801f6fec
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-11-27 23:46:20 +0900

    상세페이지 구현 및 스타일링

--- src/app/components/PostListCard.tsx
+++ src/app/components/PostListCard.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./PostModal";
 import PostDetail from "./PostDetail";
+import PoustUserAvatar from "./PoustUserAvatar";
 
 type Props = {
   post: SimplePost;
@@ -18,10 +19,7 @@ export default function PostListCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <article className="rounded-lg shadow-md border border-gray-200">
-      <div className="flex items-center p-2">
-        <Avatar image={userImage} highlight size="medium" />
-        <span className="text-gray-900 font-bold ml-2">{username}</span>
-      </div>
+      <PoustUserAvatar image={userImage} username={username} />
       <Image
         className="w-full object-cover aspect-square"
         src={image}
