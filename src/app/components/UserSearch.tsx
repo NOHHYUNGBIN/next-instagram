150181e332a0970e232440a4a4f8020544366e55
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-12-06 23:20:16 +0900

    검색 컴포넌트 구현 및 스타일링, 디바운싱 구현준비중

--- src/app/components/UserSearch.tsx
+++ src/app/components/UserSearch.tsx
@@ -4,6 +4,7 @@ import { ProfileUser } from "@/model/user";
 import React, { FormEvent, useState } from "react";
 import useSWR from "swr";
 import { GridSpinner } from "./ui/GridSpinner";
+import UserCard from "./UserCard";
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState("");
@@ -18,9 +19,10 @@ export default function UserSearch() {
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center ">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
         <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
           type="text"
           autoFocus
           placeholder="아이디나 이름을 검색하세요."
@@ -33,14 +35,14 @@ export default function UserSearch() {
       {!isLoading && !error && users?.length === 0 && (
         <p>찾는 사용자가 없습니다.</p>
       )}
-      <ul>
+      <ul className="w-full p-4 ">
         {users &&
           users.map((user) => (
             <li key={user.username}>
-              <p>{user.name}</p>
+              <UserCard user={user} />
             </li>
           ))}
       </ul>
-    </>
+    </section>
   );
 }
