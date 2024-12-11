db0907c9ce06c67a43538c292b13fa154be8a926
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-12-11 23:45:03 +0900

    사용자 포스트 컴포넌트 구현

---
+++ src/app/components/PostGrid.tsx
@@ -0,0 +1,29 @@
+import useSWR from "swr";
+import { GridSpinner } from "./ui/GridSpinner";
+import { SimplePost } from "@/model/post";
+import PostGridCard from "./PostGridCard";
+
+type Props = {
+  username: string;
+  query: string;
+};
+export default function PostGrid({ username, query }: Props) {
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useSWR<SimplePost[]>(`/api/users/${username}/${query}`);
+  return (
+    <div>
+      {isLoading && <GridSpinner />}
+      <ul>
+        {posts &&
+          posts.map((post, idx) => (
+            <li key={post.id}>
+              <PostGridCard post={post} priority={idx < 6} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
