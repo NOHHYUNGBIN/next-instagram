601b32638f821ca5bc02dbe9d1062cbdb173038b
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-12-28 21:05:55 +0900

    새포스트 작성 기본ui 구현

--- src/app/components/NewPost.tsx
+++ src/app/components/NewPost.tsx
@@ -4,6 +4,7 @@ import PoustUserAvatar from "./PoustUserAvatar";
 import FilesIcon from "./ui/icons/FilesIcon";
 import Button from "./ui/Button";
 import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 
 type Props = {
   user: AuthUser;
@@ -40,9 +41,9 @@ export default function NewPost({ user: { username, image } }: Props) {
     }
   };
   return (
-    <section>
+    <section className="w-full max-w-xl flex flex-col items-center mt-6">
       <PoustUserAvatar username={username} image={image ?? ""} />
-      <form>
+      <form className="w-full flex flex-col mt-2">
         <input
           className="hidden"
           name="input"
@@ -52,16 +53,38 @@ export default function NewPost({ user: { username, image } }: Props) {
           onChange={handleChange}
         />
         <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && "border-2 border-sky-500 border-dashed"
+          }`}
           htmlFor="input-upload"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
           onDrop={handleDrop}
         >
-          <FilesIcon />
-          <p>이미지파일을 드래그 & 드랍 하시거나 클릭하세요.</p>
+          {dragging && (
+            <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none"></div>
+          )}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FilesIcon />
+              <p>이미지파일을 드래그 & 드랍 하시거나 클릭하세요.</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
         </label>
         <textarea
+          className="outline-none text-lg border border-neutral-300"
           name="text"
           id="input-text"
           required
