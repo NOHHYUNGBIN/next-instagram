7f551437e6089f5f20a0440638b74a5465de6753
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-12-31 20:32:41 +0900

    사용자 정보확인 middleware추가

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export { default } from "next-auth/middleware";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  if (!token) {
+    if (req.nextUrl.pathname.startsWith("/api")) {
+      return new NextResponse("사용자 정보가 없습니다.", { status: 401 });
+    }
+    const { pathname, search, origin, basePath } = req.nextUrl;
+    const signInUrl = new URL(`${basePath}/auth/signin`, origin);
+    signInUrl.searchParams.append(
+      "callbackUrl",
+      `${basePath}${pathname}${search}`
+    );
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  mather: [
+    "/new",
+    "/",
+    "/api/bookmarks",
+    "/api/comments",
+    "/api/likes",
+    "/api/follow",
+    "/api/me",
+    "/api/posts/:path*",
+  ],
+};
