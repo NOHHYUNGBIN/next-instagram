276d2d4ad9dda1daa21ad01fe020a437328ce08b
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-19 16:07:11 +0900

    위치수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  unoptimized: true,
   experimental: {
     appDir: true,
   },
   images: {
+    unoptimized: true,
     domains: ["cdn.sanity.io"],
   },
 };
